cce3ca5c6688647d63f183196fe50a022979d528
michael <te0919193534@gmail.com>
2015-04-18 04:24:02 +0800

    wheel finish

--- app/src/main/java/com/musicsalvation/ChgSongView/songWheel.java
+++ app/src/main/java/com/musicsalvation/ChgSongView/songWheel.java
@@ -4,6 +4,8 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.PointF;
+import android.util.SparseArray;
 
 import com.musicsalvation.Coordinate;
 import com.musicsalvation.Graphic;
@@ -23,32 +25,24 @@ public class songWheel {
     String st[]=new String[4];
     int []x=new int[4],y=new int[4];
     int []x_now=new int[4],y_now=new int[4];
+    SparseArray<point>point=new SparseArray<>();
     public songWheel(MainActivity activity){
-        //test
-        activity.io.song_list=new JSONArray();
-        for (int i=0;i<20;i++) {
-            activity.io.song_list.put(String.valueOf(i));
-        }
-        st[0]=activity.io.song_list.optString(0);
-        st[3]=activity.io.song_list.optString(1);
-        st[2]="";
-        st[1]="";
-        //test
+
         this.activity=activity;
         tag= Graphic.LoadBitmap(activity.getResources(), R.drawable.fv_song_bar,618,223,true);
 
 
-        x[1]=1040;
-        y[1]=185;
+        x[0]=1040;
+        y[0]=185;
 
-        x[0]=970;
-        y[0]=405;
+        x[1]=970;
+        y[1]=405;
 
-        x[3]=1040;
-        y[3]=625;
+        x[2]=1040;
+        y[2]=625;
 
-        x[2]=1660;
-        y[2]=405;
+        x[3]=1660;
+        y[3]=405;
 
         x_now[0]=x[0];
         y_now[0]=y[0];
@@ -61,6 +55,14 @@ public class songWheel {
 
         x_now[3]=x[3];
         y_now[3]=y[3];
+        if (activity.io.song_list!=null) {
+            for (int i = 0; i < activity.io.song_list.length(); i++) {
+                point p = new point();
+                p.x = x[3];
+                p.y = y[3];
+                point.put(i, p);
+            }
+        }
     }
     public void draw(Canvas canvas,Paint paint){
 
@@ -68,21 +70,55 @@ public class songWheel {
             x_now[i]= Coordinate.AnalogSpeedMove(x_now[i],x[Math.abs(now+i)%4]);
             y_now[i]= Coordinate.AnalogSpeedMove(y_now[i],y[Math.abs(now+i)%4]);
             Graphic.drawPic(canvas,tag,x_now[i],y_now[i],0,255,paint);
-            Graphic.drawText(canvas,String.valueOf(i),x_now[i]-200,y_now[i],Color.WHITE,24,paint);
+        }
+        for (int k=0;k<activity.io.song_list.length();k++){
+            point p = point.get(k);
+            if (now+2>k&&now-2<k){
+                if (k==now-2||k==now+2) {
+                    p.x = Coordinate.AnalogSpeedMove(p.x,x[3] );
+                    p.y=Coordinate.AnalogSpeedMove(p.y,y[3]);
+                    point.remove(k);
+                    point.put(k,p);
+                }
+                if (k==now-1) {
+                    p.x = Coordinate.AnalogSpeedMove(p.x,x[2]-200 );
+                    p.y=Coordinate.AnalogSpeedMove(p.y,y[2]+20);
+                    point.remove(k);
+                    point.put(k, p);
+                }
+                if (k==now+1) {
+                    p.x = Coordinate.AnalogSpeedMove(p.x,x[0]-200 );
+                    p.y=Coordinate.AnalogSpeedMove(p.y,y[0]+20);
+                    point.remove(k);
+                    point.put(k,p);
+                }
+                if (k==now) {
+                    p.x = Coordinate.AnalogSpeedMove(p.x,x[1]-200 );
+                    p.y=Coordinate.AnalogSpeedMove(p.y,y[1]+20);
+                    point.remove(k);
+                    point.put(k, p);
+                }
+                Graphic.drawText(canvas,activity.io.song_list.optString(k),p.x,p.y,Color.WHITE,48,paint);
+            }else {
+                p.x = Coordinate.AnalogSpeedMove(p.x,x[3] );
+                p.y=Coordinate.AnalogSpeedMove(p.y,y[3]);
+                point.remove(k);
+                point.put(k,p);
+            }
         }
     }
     public void change(int ch){
         if (ch>0){
             if (now+2<activity.io.song_list.length()) {
-                st[(now + 2) % 4] = activity.io.song_list.optString(now + 2);
+                st[(now+1) % 4] = activity.io.song_list.optString(now + 2);
             }else {
-                st[(now + 2) % 4] ="";
+                st[(now+1) % 4] ="";
             }
         }else {
             if (now-2>=0){
-                st[(now + 2) % 4] = activity.io.song_list.optString(now - 2);
+                st[(now+1) % 4] = activity.io.song_list.optString(now - 2);
             }else{
-                st[(now+ 2) % 4] ="";
+                st[(now+1) % 4] ="";
             }
         }
         if (now+ch>=0&&now+ch<activity.io.song_list.length()) {
